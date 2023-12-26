34a140496c40ce7da7d50f9ad04b918276a35e69
YOUNTAEHEE <f_hyang@naver.com>
2023-12-26 15:47:12 +0900

    add

---
+++ src/Main.jsx
@@ -0,0 +1,8 @@
+
+export default function Main(){
+  return(
+    <div className='Main'>
+      <h1>main</h1>
+    </div>
+  );
+}
---
+++ src/UserAddress.jsx
@@ -0,0 +1,12 @@
+import { useUserQuery } from './hooks/useUserQuery';
+
+export default function UserAddress() {
+  const result = useUserQuery(2);
+  console.log(result);
+
+  return (
+    <div className="UserAddress">
+      <h1>User Address</h1>
+    </div>
+  );
+}
